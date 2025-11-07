86cc98f6e03ea5300460fbad203a315b542f3e2d
kiganoakuma <daltonhberg@icloud.com>
2025-11-06 21:22:46 -0700

    mostly working fix some ui elements, todo: get and show logs

--- internal/app/model.go
+++ internal/app/model.go
@@ -4,18 +4,29 @@ import (
 	"dockerydo/internal/app/utils"
 	"dockerydo/internal/docker"
 	"dockerydo/internal/types"
-	"dockerydo/internal/ui"
 
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func NewModel() types.Model {
 	return types.Model{
-		Table:        ui.NewTable(),
-		ShowErrPopup: false,
+		ActiveTab:       types.TabContainers,
+		PanelFoucus:     types.FocusListPanel,
+		RightPanel:      types.PanelDetails,
+		ShowErrPopup:    false,
+		RefreshEnabled:  true,
+		DetailsViewPort: viewport.New(0, 0),
+		LogsViewPort:    viewport.New(0, 0),
 	}
 }
 
 func Init(m types.Model) tea.Cmd {
-	return tea.Batch(docker.GetContainers, utils.TickCmd())
+	return tea.Batch(
+		docker.GetContainers,
+		docker.GetImages,
+		docker.GetVolumes,
+		docker.GetNetworks,
+		utils.TickCmd(),
+	)
 }
